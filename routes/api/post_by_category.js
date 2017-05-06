b20ae36314e9563dfef6d8dca22389516f7e8c9a
Efrain Gutierrez <gtz.efrain@gmail.com>
2017-05-06 12:53:42 -0500

    Formato Post

--- routes/api/post_by_category.js
+++ routes/api/post_by_category.js
@@ -1,6 +1,6 @@
 var async = require('async'),
   keystone = require('keystone');
- 
+
 var Post = keystone.list('Post');
 var PostCategory = keystone.list('PostCategory');
 
@@ -9,13 +9,13 @@ var PostCategory = keystone.list('PostCategory');
  */
 exports.list = function(req, res) {
   PostCategory.model.findOne().where('key', req.params.key).exec(function(err, item) {
-    
+
     if (err) return res.apiError('database error', err);
     if (!item) return res.apiError('not found');
 
     var category = item;
-    
-    Post.model.find().populate('author categories').where('categories').in([category.id]).exec(function(err, items) {
+
+    Post.model.find().populate('author categories').where('categories').in([category.id]).sort('-publishedDate').exec(function(err, items) {
       if (err) return res.apiError('database error', err);
       if (!items || items.length === 0) return res.apiError('not found');
 
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,7 +7,7 @@ var Post = keystone.list('Post');
  * List Posts
  */
 exports.list = function(req, res) {
-  Post.model.find().populate('author categories').exec(function(err, items) {
+  Post.model.find().populate('author categories').sort('-publishedDate').exec(function(err, items) {
 
     if (err) return res.apiError('database error', err);
 

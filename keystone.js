ac6aa240233d0dc011e687ad39a1cefa4702a0ef
Efrain Gutierrez <gtz.efrain@gmail.com>
2017-05-08 02:30:45 -0500

    mong conf

--- keystone.js
+++ keystone.js
@@ -27,7 +27,7 @@ keystone.init({
 	'user model': 'User',
 	'cookie secret': '2|@"v4%q-V8F|3PLcHJgOa%0@W!"Rc]6r]#@K{C#G;p:xs@C/"@"4RI<]!c0=97w',
 
-	'mongo': process.env.MONGO_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/suciaescena',
+	'mongo': process.env.MONGO_URI || process.env.MONGOLAB_URI || 'mongodb:suciaescena.herokuapp.com',
 	'cloudinary config': process.env.CLOUDINARY_URL || 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo'
 
 });

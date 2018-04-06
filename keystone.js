0c579446ae3bde114eb305970af37b9c0aeb8d1c
Efrain Gutierrez <gtz.efrain@gmail.com>
2018-04-06 15:29:36 -0500

    font metro df al texto

--- keystone.js
+++ keystone.js
@@ -29,17 +29,18 @@ keystone.init({
 	'mongo': process.env.MONGO_URI || process.env.MONGOLAB_URI || 'mongodb://digital_server:JRLPjZVjGIceXcY0@slna-shard-00-00-lazbm.mongodb.net:27017,slna-shard-00-01-lazbm.mongodb.net:27017,slna-shard-00-02-lazbm.mongodb.net:27017/test?ssl=true&replicaSet=SLNA-shard-0&authSource=admin' ||'mongodb://localhost/suciaescena',
 	'cloudinary config': process.env.CLOUDINARY_URL || 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo',
 
-	'ssl': true,
+	// 'ssl': true,
 	// 'ssl' : 'only',
 
-	'ssl cert':'../../etc/letsencrypt/renewal/www.slna.mx.conf',
+	// 'ssl cert':'../../etc/letsencrypt/renewal/www.slna.mx.conf',
 	// 'ssl port':process.env.PORT || 30001;
 	'trust proxy' : true,
 
 	//wysiwyg conf
 	'wysiwyg images': true,
-	'wysiwyg additional plugins': 'media mediaembed',
-	'wysiwyg additional buttons': 'media, blockquote, searchreplace'
+	'wysiwyg additional options': "{content_css : 'myLayout.css' }",
+	'wysiwyg additional plugins': 'media mediaembed  ',
+	'wysiwyg additional buttons': 'media, blockquote '
 
 });
 

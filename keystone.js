d9a7cf3541f6a166996ecc834a346081bd98a412
Efrain Gutierrez <gtz.efrain@gmail.com>
2018-04-06 12:02:00 -0500

    iframe working and post styles

--- keystone.js
+++ keystone.js
@@ -29,11 +29,11 @@ keystone.init({
 	'mongo': process.env.MONGO_URI || process.env.MONGOLAB_URI || 'mongodb://digital_server:JRLPjZVjGIceXcY0@slna-shard-00-00-lazbm.mongodb.net:27017,slna-shard-00-01-lazbm.mongodb.net:27017,slna-shard-00-02-lazbm.mongodb.net:27017/test?ssl=true&replicaSet=SLNA-shard-0&authSource=admin' ||'mongodb://localhost/suciaescena',
 	'cloudinary config': process.env.CLOUDINARY_URL || 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo',
 
-	// 'ssl': true,
+	'ssl': true,
 	// 'ssl' : 'only',
 
-	// 'ssl cert':'../../etc/letsencrypt/renewal/www.suciaescena.com.conf',
-	// 'ssl port':process.env.PORT
+	'ssl cert':'../../etc/letsencrypt/renewal/www.slna.mx.conf',
+	// 'ssl port':process.env.PORT || 30001;
 	'trust proxy' : true,
 
 	//wysiwyg conf
--- public/js/ang_mat/main.js
+++ public/js/ang_mat/main.js
@@ -100,3 +100,15 @@ config(['$routeProvider','$mdThemingProvider', function($routeProvider, $mdThemi
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 }]);
+
+angular.module('mainApp').filter('trustAs', ['$sce', 
+    function($sce) {
+        return function (input, type) {
+            if (typeof input === "string") {
+                return $sce.trustAs(type || 'html', input);
+            }
+            console.log("trustAs filter. Error. input isn't a string");
+            return "";
+        };
+    }
+]);
